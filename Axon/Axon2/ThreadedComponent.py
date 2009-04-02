4414c710148ecba43a7eb9f42b18edf9ee4eb10e
sparks.m <sparks.m@localhost>
2009-04-02 22:51:42 +0000

    For developing a new threaded component base...
    Michael.

---
+++ Axon/Axon2/ThreadedComponent.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+from Axon2.Scheduler import scheduled
+
+class component(scheduled):
+    Inboxes = {
+        "inbox" : "default inbox",
+        "control" : "default control inbox",
+    }
+    Outboxes = {
+        "outbox" : "default outbox",
+        "signal" : "default control signal outbox",
+    }
+    def __init__(self,  *argv, **argd):
+        self.inboxes = { }
+        for boxname in self.Inboxes:
+            self.inboxes[boxname] = []
+        self.outboxes = { }
+        super(component,self).__init__()
+
+    def activate(self, noschedule=False):
+        self.next = self.main().next
+        return self
+
+    def send(self, message,outbox): # 461ms vs 186   
+        recipient,inbox = self.outboxes[outbox]
+        recipient.deliv(message,inbox)
+
+    def deliv(self, message,inbox): # 461ms vs 186   
+        self.inboxes[inbox].append(message)
+        self.s.schedule(self.tick)
+
+    def post(self, message, (recipient, inbox)):
+        self.link((self, "__temporary__"), (recipient, inbox))
+        self.send(message, "__temporary__")
+        self.unlink( (self, "__temporary__") )
+
+    def reschedule(self, R):
+        self.s.schedule(self.tick)
+
+    def run(self):
+        self.s.run()
+
+    def tick(self):
+        try:
+            R = self.next()
+            if hasattr(R, "handle"):
+               R.handle(self)
+            elif R:
+               self.reschedule(R)
+        except StopIteration:
+            pass
+
+    def link(self, (sender, outbox), reciever):
+        sender.outboxes[outbox] = reciever
+
+    def unlink(self, (sender, outbox)):
+        del sender.outboxes[outbox]
+
+    def recv(self, inbox="inbox"):
+        return self.inboxes[inbox].pop(0)
