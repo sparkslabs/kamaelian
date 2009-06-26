9d82311d148230359e34a53d61359b88aebbd62f
sparks.m <sparks.m@localhost>
2009-06-26 08:43:35 +0000

    Packaging files for convenience

---
+++ Axon/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+
+from distutils.core import setup
+
+setup(name = "Axon2",
+      version = "0.0.0-destruct0",
+      description = "Axon 2 - a branch for re-engineering Axon, with a view of consolidation & optimisations",
+      author = "Michael Sparks",
+      author_email = "sparks.m@gmail.com",
+      url = "",
+      license = "Copyright (c)2009 Michael Sparks. New BSD license",
+      packages = [\
+                  "Axon2",
+                  ""],
+      long_description = """\
+Axon 2
+------
+
+A branch for re-engineering Axon, with a view of consolidation & optimisations
+""",
+      )
